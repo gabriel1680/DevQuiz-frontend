fe510c5ed589b2d09772377e9d9c5e99df4de0a6
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-09-17 19:17:52 -0300

    feat: make answer list shuffled

--- src/gateways/axios/AxiosRandomQuestionsGateway.ts
+++ src/gateways/axios/AxiosRandomQuestionsGateway.ts
@@ -3,16 +3,20 @@ import { v4 as uuid } from "uuid";
 
 import { Question } from "../../types/Quiz";
 import { RandomQuestionsGateway } from "../RandomQuestionsGateway";
+import { randomQuestionsApiMock } from "../../utils/mock";
 
 export class AxiosRandomQuestionsGateway implements RandomQuestionsGateway {
   constructor(private readonly api: AxiosInstance) {}
 
   private buildParams() {
-    return "?amount=2&category=9&difficulty=easy&type=multiple";
+    return "?amount=10&type=multiple";
   }
 
   async getRadomQuestions(): Promise<Question[]> {
-    const { data } = await this.api.get<{ response_code: number, results: any[] }>(this.buildParams());
+    const { data } = await this.api.get<{
+      response_code: number;
+      results: typeof randomQuestionsApiMock;
+    }>(this.buildParams());
     return data.results.map((question, idx) => {
       const questionId = uuid();
       return {
@@ -20,9 +24,10 @@ export class AxiosRandomQuestionsGateway implements RandomQuestionsGateway {
         ...question,
         text: question.question,
         index: idx + 1,
-        answers: [...question.incorrect_answers, question.correct_answer].map(
-          answer => ({ id: uuid(), questionId, text: answer })
-        ),
+        answers: [...question.incorrect_answers, question.correct_answer]
+          .map(answer => ({ id: uuid(), questionId, text: answer }))
+          // shuffle
+          .sort(() => 0.5 - Math.random()),
         correctAnswer: question.correct_answer,
       };
     });
